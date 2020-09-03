fe93517f4f5a307be01592ab866282c414ab2ea4
Tomas Agrimbau <tomas@amalgama.co>
2020-09-02 23:42:15 -0300

    Add set env to deevelopment

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -6,7 +6,7 @@ set :deploy_to, '/srv/rails/iflan-ws'
 
 server 'api.iflan.house', user: 'deploy', roles: %w{web app}
 
-set :rails_env, 'staging'
+set :rails_env, 'development'
 set :branch, 'master'
 
 #after 'deploy', 'sitemap:refresh'
