2b58be87305dee10cc8fbd0041855b8784cdcaf5
Tomas Agrimbau <tomas@amalgama.co>
2020-09-02 22:53:29 -0300

    Add active admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,20 +1,13 @@
-ActiveAdmin.register AdminUser do
+ActiveAdmin.register User do
   permit_params :email, :password, :password_confirmation
 
   index do
     selectable_column
-    id_column
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
     actions
   end
 
   filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
 
   form do |f|
     f.inputs do
